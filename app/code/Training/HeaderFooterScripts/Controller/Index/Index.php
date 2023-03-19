281d448c6c4bbe42a98bb94045c41e05f8b96956
SergM2014 <weisse011@gmail.com>
2023-03-19 10:09:14 +0200

    17

---
+++ app/code/Training/HeaderFooterScripts/Controller/Index/Index.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Training\HeaderFooterScripts\Controller\Index;
+
+use Magento\Framework\App\ActionInterface;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index implements ActionInterface
+{
+    public function __construct(private PageFactory $pageFactory)
+    {}
+
+    public function execute()
+    {
+        return $this->pageFactory->create();
+    }
+}
+

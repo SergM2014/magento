f10d016c16a6d22eca9f74b96b501489094e5d64
SergM2014 <weisse011@gmail.com>
2023-03-29 15:02:16 +0300

    24

---
+++ app/code/Training/ReplaceConstructorArguments/Model/UpperCaseName.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Training\ReplaceConstructorArguments\Model;
+
+class UpperCaseName extends DefaultName
+{
+   
+
+    public function getName(): string
+    {
+        return strtoupper(parent::getName());
+    }
+}
